5ee6149eed2a701e7fba7553e315ad4fadc3da08
bialx <40208818+bialx@users.noreply.github.com>
2018-06-12 20:14:52 +0200

    Update koko.py

--- koko.py
+++ koko.py
@@ -5,11 +5,13 @@ from tkMessageBox import *
 import pandas as pd
 import numpy as np
 
-#chemin par default
+#chemin par default a modifier pour chez vous
 path = r"/home/jeremie/log.xls"
 
+#initialise notre fenetre principale tkinter
 fenetre = Tk()
 
+#On veut juste donner la possibilite a l'utilisateur de choisir ou enregistrer son fichier xls
 def NewPath():
     top = Toplevel(fenetre)
     Path = Label(top, text = 'Path : (ex : r"/home/jeremie/log.xls")')
@@ -22,6 +24,7 @@ def NewPath():
     quit.grid ()
 
 
+#Cree menu tkinter
 menu = Menu(fenetre)
 fenetre.config(menu=menu)
 filemenu = Menu(menu)
@@ -31,22 +34,21 @@ filemenu.add_command(label="Path", command=NewPath)
 
 champ_label = Label(fenetre, text="Fiche d'utilisation de l'ordinateur")
 
+#demande avec un pop up si l'utilisateur a bien rempli le formulaire
 def callback():
     if askyesno('', 'Avez vous rempli le formulaire ?'):
         fenetre.destroy()
     else:
         showinfo('', 'Remplissez le formulaire')
 
-
-Nom = Label(fenetre, text = 'Nom : ')
-Nom=StringVar()
-
+#On cree le champ de saisie Nom 
 Nom = Label(fenetre, text = 'Nom : ')
 Nom.grid(column=0, row=0, sticky='w')
-Nom=StringVar()
+Nom = StringVar()
 Champ = Entry(fenetre, textvariable= Nom, width=31)
 Champ.grid(column=1, row=0, sticky='sw', columnspan=2, padx=10)
 
+#On cree le champ de saisie Prenom 
 Prenom = Label(fenetre, text = 'Prenom : ',)
 Prenom.grid(column=0, row=1,sticky='w',pady=2)
 Prenom=StringVar()
@@ -54,9 +56,11 @@ Champ2 = Entry(fenetre, textvariable= Prenom, width=31)
 Champ2.grid(column=1, row=1,columnspan=2)
 
 
-
+#la fonction permettant d'ecrire dans le fichier xls
+#on utilise la librairie panda pour recuperer les donnees du fichier xls, on crer ensuite un dataframe avec le nom/prenom rentre
+#par l'utilisateur, et on concatene le dataframe du xls et celui nom/prenom. On ecrase ensuite le fichier xls avec le new dataframe
 def envoyer():
-    df = pd.read_excel(path,encoding='utf-8')
+    df = pd.read_excel(path,encoding='utf-8') 
     data = pd.Series([Nom.get()])
     data2 = pd.Series([Prenom.get()])
     df1 = pd.DataFrame({'Nom':data, 'Prenom':data2})
@@ -66,6 +70,7 @@ def envoyer():
     df2.to_excel(writer, sheet_name='Sheet1')
     writer.save()
 
+ #On cree les boutons interactifs
 Fermer = Button(fenetre, text="Fermer", command=callback, pady = 2)
 Fermer.grid (column=1, row=11,sticky='sw', pady=20)
 Envoyer = Button(fenetre, text="Envoyer", command=envoyer, pady = 2)
